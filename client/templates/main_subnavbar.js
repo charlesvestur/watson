9dfc41186bed7593ad97f8ff9cdc6e97376fbf59
Charles Vestur <charles.vestur@gmail.com>
2015-06-04 00:14:45 +0200

    Did sort with New and Best

--- client/templates/main_subnavbar.js
+++ client/templates/main_subnavbar.js
@@ -4,9 +4,21 @@ UI.registerHelper("currentRouteName",function(){
 	   case "mainSymptomChecker":
 	       className="stetoscope-active";
 	       break;
-	   case ("mainResults" || "newDiscussions" || "bestDiscussions"):
+	   case "mainResults":
 	       className="results-active";
 	       break;
+	   case "newDiscussions":
+	       className="results-active";
+	       break;	
+	   case "discussionSubmit":
+	       className="results-active";
+	       break;
+	   case "discussionPage":
+	       className="results-active";
+	       break;
+	   case "discussionEdit":
+	       className="results-active";
+	       break;   
 	   case "mainFeedback":
 	   		className="feedback-active";
 	   		break;
